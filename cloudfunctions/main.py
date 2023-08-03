2abd4c1d08f50c0e9d8d5a1b8f0dae0087814e08
Sean Boren <smboren2@gmail.com>
2023-08-03 19:57:09 +0000

    adding cloudfunctions

---
+++ cloudfunctions/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from os import getenv
+
+from api import general_use as gen
+
+def get_field():
+    dg_api = getenv("DG_API")
+    field = gen.get_field_updates(**{'file_format':'csv', 'key': dg_api})
+
+    print(field.head(5))
+    
+    return "OK"
