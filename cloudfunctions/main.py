f2879e9759f0832702c7880c9c82af8550f0230c
Sean Boren <smboren2@gmail.com>
2023-08-03 21:55:46 +0000

    adding request as parameter to main.py

--- cloudfunctions/main.py
+++ cloudfunctions/main.py
@@ -3,7 +3,7 @@ from os import getenv
 
 from api import general_use as gen
 
-def get_field():
+def get_field(request):
     dg_api = getenv("DG_API")
     field = gen.get_field_updates(**{'file_format':'csv', 'key': dg_api})
 
